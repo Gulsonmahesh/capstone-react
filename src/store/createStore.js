476c3c2ba2fa6f7a1181425eaf1bdf1491a07eaa
Gulsonmahesh <maheshqualis@gmail.com>
2020-12-24 17:05:11 +0530

    Re enable thunk for async action

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,7 +1,7 @@
-// import { applyMiddleware, compose, createStore } from 'redux';
-// import thunk from 'redux-thunk';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 // import { getFirebase } from 'react-redux-firebase'
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 import RootReducer from './reducer/rootReducer';
 
 const initialState = {}
@@ -9,8 +9,8 @@ const initialState = {}
 // const middlewares = [
 //   thunk.withExtraArgument(getFirebase)
 // ]
-// const store = createStore(RootReducer,initialState,compose(applyMiddleware(...middlewares)));
+const store = createStore(RootReducer,initialState,compose(applyMiddleware(thunk)));
 
-const store = createStore(RootReducer,initialState)
+// const store = createStore(RootReducer,initialState)
 
 export default store;
